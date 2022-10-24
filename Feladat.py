65ea2497b7b038954c160cd1bccd69133c63fc70
Levente <116150961+DLevente@users.noreply.github.com>
2022-10-24 09:40:37 +0200

    Add files via upload

---
+++ Feladat.py
@@ -0,0 +1,14 @@
+#1.Feladat: Kérjünk be 5 db egész számot és nézzük meg hogy melyik páros és páratlan.
+
+x = 1
+
+while x < 5:
+    
+    szam = int(input("Kérem a számokat"))
+    
+    if szam % 2 == 0:
+        print("A szám páros")
+    else:
+        pint("A szám páratlan")
+        
+    x += 1
